f3a9e52f1890fecb4c4cf33c73201e41e94eba96
Apirak Fakin <apirakfakin@gmail.com>
2024-05-21 14:07:31 +0700

    feat: add ex-6

--- ex-6.js
+++ ex-6.js
@@ -1,12 +1,24 @@
 const carCollection = ["toyota", "fiat", "honda", "bmw"];
 
 function updateCarCollection(carBrand) {
-  // Start coding here
+  let result;
+  if (carCollection.includes(carBrand)) {
+    let carIndex;
+    carIndex = carCollection.indexOf(carBrand) + 1
+    result = `${carBrand} already exists in position ${carIndex} of the car collection.`
+  } else {
+    carCollection.push(carBrand)
+    let newCarCollection = carCollection.join(", ")
+    result = `New car collection is: ${newCarCollection}`
+  }
+  return result
 }
 
 //ผลลัพธ์ที่ควรได้จาก Example case
 //ในกรณีที่ยังไม่มียี่ห้อรถใน carCollection
-console.log(updateCarCollection("audi")); //new car collection is : toyota,fiat,honda,bmw,audi.
+console.log(updateCarCollection("audi"));
+//new car collection is : toyota,fiat,honda,bmw,audi.
 
 //ในกรณีที่มียี่ห้อรถใน carCollection
-console.log(updateCarCollection("toyota")); //toyota has already existed in the 1 position of car collection.
+console.log(updateCarCollection("toyota"));
+//toyota has already existed in the 1 position of car collection.
